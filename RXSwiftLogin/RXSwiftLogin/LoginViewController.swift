be456f7cc2602977586b109252c5a906fb8ed966
Perfor <913661647@qq.com>
2017-08-30 16:53:57 +0800

    导入rxswift rxcoco，绘制界面

---
+++ RXSwiftLogin/RXSwiftLogin/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  RXSwiftLogin
+//
+//  Created by Kim on 2017/8/30.
+//  Copyright © 2017年 Brain. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var usernameTipLabel: UILabel!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var passwordTipLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- RXSwiftLogin/RXSwiftLogin/ViewController.swift
+++ RXSwiftLogin/RXSwiftLogin/ViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var secondPasswordTextField: UITextField!
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
+    @IBOutlet weak var usernameTipLabel: UILabel!
+    
+    @IBOutlet weak var passwordTipLabel: NSLayoutConstraint!
+    
+    @IBOutlet weak var secondPasswordTipLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func registerButtonPress(_ sender: Any) {
     }
 
 
